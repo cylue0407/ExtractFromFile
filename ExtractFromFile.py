1465f7e3a3e94722b3b9526675a4174419d897c2
cylue0407 <fakeprogrammer19900407@gmail.com>
2020-08-10 17:16:36 +0800

    02_TestFunction

--- ExtractFromFile.py
+++ ExtractFromFile.py
@@ -2,9 +2,11 @@
 import RegularEx
 import ReadFile
 
-rawText = ReadFile.getTextFromDoc('test.docx')
-print(rawText)
-if RegularEx.reEXBirth(rawText):
-    print("Match Success")
-else:
-    print("Match Failed.")
+rawText = ReadFile.getTextFromDoc("test.docx")
+#print(rawText)
+#if RegularEx.reEXEmail(rawText):
+#    print("Match Success")
+#else:
+#    print("Match Failed.")
+#RegularEx.reEXEmail("My email is: xx@163.com & xx@gmail.com")
+RegularEx.reEXEmail(rawText)
--- RegularEx.py
+++ RegularEx.py
@@ -8,10 +8,14 @@ def reEXBirth(usrInput):
 
 #match Email
 def reEXEmail(usrInput):
-    pattern_list = ['^.*@163.com$','','']
-    if(re.match(pattern, usrInput)):
-        re.findall(pattern, usrInput)
-    
+    pattern_list = ['^.*@126\.com$','^.*@139\.com$','^.*@163\.com.?','^.*@sina\.com$','^.*@qq\.com$','^.*@sohu\.com$','^.*@zohomail\.com$','^.*@189\.com$','^.*@yeah\.net$','^.*@.+\.cn$',]
+    matchStr = ''
+    for pattern in pattern_list:
+        print(pattern)
+        if(re.match(pattern, usrInput)):
+            #matchStr += re.findall(pattern, usrInput)
+            print(re.findall(pattern,usrInput))
+    result = matchStr
     return result
 
 #match phone number+
